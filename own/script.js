c4b771882657b609ed80bf8cccfb16c1632d7e44
emilundg <lundgrenemil@hotmail.com>
2018-03-11 15:17:27 +0100

    FINISHED THIS SH

--- own/script.js
+++ own/script.js
@@ -1,7 +1,7 @@
-$(function() {
+var glyphs = document.getElementById('stats');
+var desc = document.getElementById('stats2');
 
-    var glyphs = document.getElementById('stats');
-    var desc = document.getElementById('stats2');
+$(function() {
 
   $(desc).fadeOut('slow', function() {
     $(glyphs).fadeTo("slow", 1.0);
@@ -15,40 +15,60 @@ $(function() {
     }
   });
 
-  $('.btn1').click(function () {
+  $('.btn1').click(function() {
     $(glyphs).fadeOut('slow', function() {
-    $(desc).fadeTo("slow", 1.0);
+      $(desc).fadeTo("slow", 1.0);
     });
   });
 
-  $('.btn2').click(function () {
+  $('.btn2').click(function() {
     $(desc).fadeOut('slow', function() {
-    $(glyphs).fadeTo("slow", 1.0);
+      $(glyphs).fadeTo("slow", 1.0);
     });
   });
 });
 
+var sometext = document.getElementById("overlaySome");
+
+function highlightBox() {
+  // d3.selectAll("#impress").transition().style("border","solid red 3px");
+  var glyphs = document.getElementById("glyphs").style.border = "solid rgb(23, 144, 46) 3px";
+  // insert fadeIn of div thats places correctly
+
+  var af = document.getElementById("affected_individuals").style.border = "solid rgb(23, 144, 46) 3px";
+
+  var type = document.getElementById("types_of_attacks").style.border = "solid rgb(23, 144, 46) 3px";
+
+  var number = document.getElementById("Breaches_per_capita").style.border = "solid rgb(23, 144, 46) 3px";
+
+  $(sometext).fadeTo("slow", 1);
+}
+
+function unhighlightBox() {
+  d3.selectAll(".step").transition().style("border", null);
+  $(sometext).fadeTo("slow", 0)
+}
+
 function showAnnotations()
 {
-  d3.selectAll(".annotation").style("display", "block");
+  d3.selectAll(".annotation").style("opacity", "1");
 }
 
 function hideAnnotations()
 {
-  d3.selectAll(".annotation").style("display", "none");
+  d3.selectAll(".annotation").style("opacity", "0");
 }
 
-var nextBoxToHilight = 1;
-function highlightBox()
-{
-  // d3.selectAll("#impress").transition().style("border","solid red 3px");
-  var glyphs = document.getElementById("glyphs").style.border = "solid red 3px";
-  // insert fadeIn of div thats places correctly
-
-}
-
-function unhighlightBox()
-{
-  d3.selectAll(".step").transition().style("border",null);
-  nextBoxToHilight = 1;
-}
+$(window).bind('hashchange', function() {
+ /* things */
+ var lastpath = (location.href.match(/([^\/]*)\/*$/)[1]);
+ if (lastpath === "overview" || lastpath === "affected_individuals" || lastpath === "types_of_attacks" ||  lastpath === "Breaches_per_capita") {
+   document.getElementById('glyph_af1').style.opacity = "1";
+   document.getElementById('type_anno1').style.opacity = "1";
+   document.getElementById('map_anno1').style.opacity = "1";
+ } else {
+   document.getElementById('glyph_af1').style.opacity = "0";
+    document.getElementById('type_anno1').style.opacity = "0";
+   document.getElementById('map_anno1').style.opacity = "0";
+ }
+});
